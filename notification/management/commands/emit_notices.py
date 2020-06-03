24b5fcbc4c4e4694658261dc545f213462416f83
eric <eric@hellman.net>
2020-06-03 17:43:32 -0400

    Update emit_notices.py

--- notification/management/commands/emit_notices.py
+++ notification/management/commands/emit_notices.py
@@ -8,7 +8,7 @@ from notification.engine import send_all
 class Command(BaseCommand):
     help = "Emit queued notices."
     
-    def handle_noargs(self, **options):
+    def handle(self, **options):
         logging.basicConfig(level=logging.DEBUG, format="%(message)s")
         logging.info("-" * 72)
         send_all()
